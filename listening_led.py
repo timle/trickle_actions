fc305bf9ae199d7a3df8352dcd2844cda0e7b459
neeilan <neeilans@live.com>
2016-11-20 01:13:54 -0500

    change wait time back to 6

--- listening_led.py
+++ listening_led.py
@@ -3,11 +3,7 @@ import time
 GPIO.setmode(GPIO.BCM) #sets pin numbering, dont change
 
 GPIO.setwarnings(False) # i dunno
-
 GPIO.setup(19,GPIO.OUT) # tells pi that we want to use this port
-
 GPIO.output(19,GPIO.HIGH) #set output on to high
-time.sleep(2.5)
-print "LED off"
 time.sleep(6)
 GPIO.output(19,GPIO.LOW) # set output to low)
