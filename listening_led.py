f31a198daa8f2585ed047e2da56d3e618ee163c6
neeilan <neeilans@live.com>
2016-11-20 00:44:34 -0500

    Update listening_led.py

--- listening_led.py
+++ listening_led.py
@@ -7,6 +7,7 @@ GPIO.setwarnings(False) # i dunno
 GPIO.setup(19,GPIO.OUT) # tells pi that we want to use this port
 
 GPIO.output(19,GPIO.HIGH) #set output on to high
-time.sleep(4) # wait 4  seconds
+time.sleep(2.5)
 print "LED off"
+time.sleep(6)
 GPIO.output(19,GPIO.LOW) # set output to low)
