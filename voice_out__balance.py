6c0762eb29b6b52046010cce28331b3362951240
timle <tsk@yorku.ca>
2016-11-19 22:08:36 -0500

    how many sleeps added

--- voice_out__balance.py
+++ voice_out__balance.py
@@ -1,13 +1,33 @@
 import os
 import sys
 
-print sys.argv[1]
-
-print 'pico2wave -w test.wav "' + sys.argv[1] + '" && aplay test.wav'
-
-os.system('pico2wave -w test.wav "' + sys.argv[1] + '" && aplay test.wav')
+import requests
 
 
 
+url_bal = 'https://trickle-f05bc.firebaseio.com/kid/balance.json'
 
+#url_1 = 'https://trickle-f05bc.firebaseio.com/kid.json'
+
+response = requests.get(url_bal)
+bal_val = response.json()
+
+text_to_read = "I'm sorry, I my pigsternet isn't working right now. Please try again"
+
+if bal_val == 0:
+    text_to_read = "Your account is empty!"
+
+if (bal_val > 0) and (bal_val < 1):
+    text_to_read = "You have " + str(bal_val) + "cents in your account."
+
+if (bal_val == 1):
+    text_to_read = "You have " + str(int(bal_val)) + " dollar in your account."
+
+if (bal_val > 1):
+    text_to_read = "You have over " + str(int(bal_val)) + " dollars in your account. You are a great saver, keep it up!"
+
+#switch this on deployment
+
+#os.system('pico2wave -w test.wav "' + sys.argv[1] + '" && aplay test.wav')
+print('pico2wave -w test.wav "' + text_to_read + '" && aplay test.wav')
 
